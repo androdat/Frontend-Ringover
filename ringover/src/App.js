d1f5be68a358647caef960b896294e870d070273
androdat <saxenavedant61@gmail.com>
2022-08-09 07:29:41 +0530

    Testing....

--- ringover/src/App.js
+++ ringover/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Quickview from "./components/Quickview";
+import QuickView from "./components/QuickView";
 import RingoverSalesforce from "./components/RingoverSalesforce";
 import { MyContext } from "./contexts/MyContext";
 function App() {
@@ -9,26 +9,52 @@ function App() {
   const [leadsMatched, setLeadsMatched] = useState(0);
   const [highlightId, sethighlightId] = useState("");
   const [highlightId2, sethighlightId2] = useState("xyz");
+  const [showAcView, setShowAcView] = useState(null);
+  const [acTab, setAcTab] = useState(false);
+  const [performAction, setPerformedAction] = useState("");
+  const [revertAction, setRevertAction] = useState("");
+  const [sfTotal, setSfTotal] = useState("");
+  const [disabledFields, setDisabledFields] = useState([]);
+  const [frr, setFrr] = useState(0);
+  const [contactClickedState, setContactClickedState] = useState(false);
+  const [spControllVariable, setSpControllVariable] = useState(false);
+  const providerValues = {
+    leadsTotal,
+    setLeadsTotal,
+    leadsMatched,
+    setLeadsMatched,
+    highlightId,
+    sethighlightId,
+    highlightId2,
+    sethighlightId2,
+    showAcView,
+    setShowAcView,
+    acTab,
+    setAcTab,
+    performAction,
+    setPerformedAction,
+    revertAction,
+    setRevertAction,
+    sfTotal,
+    setSfTotal,
+    disabledFields,
+    setDisabledFields,
+    frr,
+    setFrr,
+    contactClickedState,
+    setContactClickedState,
+    spControllVariable,
+    setSpControllVariable,
+  };
   return (
     <div className="App">
       <div className="grid-container">
-        <MyContext.Provider
-          value={{
-            leadsTotal,
-            setLeadsTotal,
-            leadsMatched,
-            setLeadsMatched,
-            highlightId,
-            sethighlightId,
-            highlightId2,
-            sethighlightId2,
-          }}
-        >
+        <MyContext.Provider value={providerValues}>
           <div className="grid-item grid-item-1">
             <Header />
           </div>
           <div className="grid-item grid-item-2">
-            <Quickview />
+            <QuickView />
           </div>
           <div className="grid-item grid-item-3">
             <RingoverSalesforce />
@@ -40,9 +66,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <Header />
-      <Quickview/> 
-      <RingoverSalesforce/> */
-}
